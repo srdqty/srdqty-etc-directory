07c6d074edbb163abef44c630c65697b2ad3f1f3
Sophia Donataccio <srdqty@gmail.com>
2017-09-05 23:26:44 -0400

    Extracts common package overrides to separate nix file

---
+++ nixos/package-overrides/common/default.nix
@@ -0,0 +1,70 @@
+pkgs: {
+  icestorm = pkgs.icestorm.overrideAttrs (oldAttrs: rec {
+    name = "icestorm-${version}";
+    version = "2017.08.01";
+    src = pkgs.fetchFromGitHub {
+      owner = "cliffordwolf";
+      repo = "icestorm";
+      rev = "873eb9effaef6f97df24ca8d7b6eae3721303454";
+      sha256 = "1ywyalqavlj3134xsa6p95xqmdgjw4ixndkxfp77yk3bfs04sxl7";
+    };
+  });
+
+  arachne-pnr = pkgs.arachne-pnr.overrideAttrs (oldAttrs: rec {
+    name = "arachne-pnr-${version}";
+    version = "2017.06.29";
+    src = pkgs.fetchFromGitHub {
+      owner = "cseed";
+      repo = "arachne-pnr";
+      rev = "7e135edb31feacde85ec5b7e5c03fc9157080977";
+      sha256 = "1wszcx6hgw4q4r778zswrlwdwvwxq834bkajck8w9yfqwxs9lmq8";
+    };
+  });
+
+  yosys = pkgs.yosys.overrideAttrs (oldAttrs: rec {
+    name = "yosys-${version}";
+    version = "2017.08.14";
+    src = pkgs.fetchFromGitHub {
+      owner = "cliffordwolf";
+      repo = "yosys";
+      rev = "2cf0b5c1575b2ea4f8d949854eab7ab2bfdcd10e";
+      sha256 = "00sfkq9z7g0ga5x3xphagyrvxrqafkd4429bx39lxwkm8l302w6h";
+    };
+  });
+
+  youtube-dl = pkgs.youtube-dl.overrideAttrs (oldAttrs: rec {
+    name = "youtube-dl-${version}";
+    version = "2017.08.13";
+    src = pkgs.fetchurl {
+      url = "https://yt-dl.org/downloads/${version}/${name}.tar.gz";
+      sha256 = "212c4e6a622d4e20ea888d1b1539ab063e171bed2a05c9e00db71ed53ab059dd";
+    };
+  });
+
+  lastpass-cli = pkgs.lastpass-cli.overrideAttrs (oldAttrs: rec {
+    version = "1.2.1";
+    src = pkgs.fetchFromGitHub {
+      owner = "lastpass";
+      repo = "lastpass-cli";
+      rev = "eda59f8b9a064c38b8a6d3093695ee0d29afe837";
+      sha256 = "0nrsrd5cqyv2zydzzl1vryrnj1p0x17cx1rmrp4kmzh83bzgcfvv";
+    };
+  });
+
+  img2txt = pkgs.callPackage ../../custom-packages/img2txt {
+    inherit (pkgs) fetchFromGitHub licenses;
+    inherit (pkgs.pythonPackages) buildPythonPackage docopt pillow;
+  };
+
+  aseprite = pkgs.callPackage ../../custom-packages/aseprite { };
+
+  sql-workbench = pkgs.callPackage ../../custom-packages/sql-workbench {
+    mysql-jdbc = pkgs.callPackage ../../custom-packages/mysql-jdbc { };
+    postgresql-jdbc = pkgs.callPackage ../../custom-packages/postgresql-jdbc { };
+    redshift-jdbc = pkgs.callPackage ../../custom-packages/redshift-jdbc { };
+
+    inherit (pkgs) stdenv fetchurl jre unzip;
+  };
+
+  kubectl = pkgs.callPackage ../../custom-packages/kubectl { };
+}
